b663a9ff7b16d009b6d75b54c8fbcad94300b24d
Bo Kristensen <skriv@tilbo.dk>
2024-02-14 11:38:44 +0100

    footer

--- app/ui/footer/Footer.jsx
+++ app/ui/footer/Footer.jsx
@@ -1,5 +1,9 @@
+import styles from "./footer.module.css"
+
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <div className={styles.container}>
+            <p>Grundejerforening for Lindholt Etape 2 - <a href="mailto:skriv@tilbo.dk"><span className="mailLink">skriv@tilbo.dk</span></a></p>
+        </div>
     )
 }
