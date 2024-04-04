19501e972041db11af3feab95d833823b5b51ca0
Bo Kristensen <skriv@tilbo.dk>
2024-04-04 22:26:02 +0200

    Fixed mail adresser

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,3 +1,5 @@
 export default function Contact() {
-    return <p className="notReady">Denne side er ikke færdig endnu!</p>
+    return <>
+        <p className="notReady">Fejl og mangler kan sendes hertil:  <a href="mailto:fejlmangler@lindholt2.dk"><span className="mailLink">fejlmangler@lindholt2.dk</span></a></p>
+    </>
 };
--- app/ui/footer/Footer.jsx
+++ app/ui/footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css"
 export default function Footer() {
     return (
         <div className={styles.container}>
-            <p>Grundejerforening for Lindholt Etape 2 - <a href="mailto:skriv@tilbo.dk"><span className="mailLink">skriv@tilbo.dk</span></a></p>
+            <p>Grundejerforening for Lindholt Etape 2 - Bestyrelse kan kontaktes på: <a href="mailto:bestyrelse@lindholt2.dk"><span className="mailLink">bestyrelse@lindholt2.dk</span></a></p>
         </div>
     )
 }
